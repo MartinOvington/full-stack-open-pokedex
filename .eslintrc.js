6f76c1d5069345642a44c066a97cbb25d6629bbf
Martin Ovington <MartinOvington@msn.com>
2022-06-22 08:15:06 +0100

    ex11.6 fixed linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,9 +15,9 @@ module.exports = {
   plugins: ['react', 'jest'],
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
-    semi: ['error', 'never'],
+    semi: ['error', 'always'],
     eqeqeq: 'error',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
